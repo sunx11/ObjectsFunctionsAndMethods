3707e4e9a6fcaa0cd462ed915597da5068f6fab1
sunx1 <sunx1@rose-hulman.edu>
2019-06-09 12:54:57 +0800

    done

--- src/m6_using_objects.py
+++ src/m6_using_objects.py
@@ -146,7 +146,7 @@ def lines():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # done: 4. Implement and test this function.
     # ------------------------------------------------------------------
 
 
